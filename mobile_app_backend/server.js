8f95b70fddbdc6f51e253822efbfbcb253f36dc1
Chamath Fernando <chamadula555@gmail.com>
2024-11-19 17:33:49 +0530

    Final Commit

--- mobile_app_backend/server.js
+++ mobile_app_backend/server.js
@@ -10,12 +10,26 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Incoming request:', {
+    method: req.method,
+    path: req.path,
+    url: req.url,
+    params: req.params
+  });
+  next();
+});
+
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const subordinateRoutes = require('./routes/subordinateRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const leaveRoutes = require('./routes/leaveRoutes');
 const orderStatusRoutes = require('./routes/orderStatusRoutes');
+const salaryRoutes = require('./routes/salaryRoutes');
+const employeeRoutes = require('./routes/employeeRoutes');
+
+
 
 
 
@@ -24,8 +38,11 @@ const orderStatusRoutes = require('./routes/orderStatusRoutes');
 app.use('/api', authRoutes);
 app.use('/api', subordinateRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/', leaveRoutes);
+app.use('/api', leaveRoutes);
 app.use('/api', orderStatusRoutes);
+app.use('/api', salaryRoutes);
+app.use('/api', employeeRoutes);
+
 
 // Basic error handler
 app.use((err, req, res, next) => {
